1656f0a324175a58be0294f7f238a179c1c8b722
rumankl <rumankrishnalohala@gmail.com>
2025-06-19 14:29:09 +0545

    modify the api of backend from render

--- src/data/apis.js
+++ src/data/apis.js
@@ -1,4 +1,4 @@
 
 // export const base = "http://192.168.1.77:5001";
-export const base = "https://pet-shop-44nv.onrender.com";
+export const base = "https://pet-shop-5jnr.onrender.com";
 export const baseUrl = `${base}/api`

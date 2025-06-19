8a98011f09a60bf839639a679b54f171ca8a9db1
rumankl <rumankrishnalohala@gmail.com>
2025-06-19 17:42:07 +0545

    some changes

--- src/data/apis.js
+++ src/data/apis.js
@@ -1,4 +1,4 @@
 
-export const base = "http://192.168.0.101:5001";
-// export const base = ["https://pet-shop-5jnr.onrender.com"];
+// export const base = "http://192.168.0.101:5001";
+export const base = ["https://pet-shop-5jnr.onrender.com"];
 export const baseUrl = `${base}/api`

d172c0e8fd7b8c240f4e86cf06e90b1fc449e10e
rumankl <rumankrishnalohala@gmail.com>
2025-06-19 18:30:42 +0545

    some changes

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(cors(
   {
     // origin: ['https://pet-shop-xi-taupe.vercel.app', 'http://localhost:3000'],
-    origin: 'https://pet-shop-xi-taupe.vercel.app',
+    origin: ['https://pet-shop-xi-taupe.vercel.app'],
     credentials: true
   }
 ));

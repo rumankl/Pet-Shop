39997bcc1ded14316ed1ed45ddfac8bc348a6a98
rumankl <rumankrishnalohala@gmail.com>
2025-06-19 22:28:07 +0545

    change

--- src/data/apis.js
+++ src/data/apis.js
@@ -1,5 +1,4 @@
 
 // export const base = "http://192.168.1.77:5001";
-export const base = process.env.REACT_APP_API_URL;
-export const baseUrl = `${base}/api`;
-
+export const base = "https://pet-shop-hqmu.onrender.com";
+export const baseUrl = `${base}/api`

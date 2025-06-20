5833f171311f81bfe8969991883b62b5f1dff1a8
rumankl <rumankrishnalohala@gmail.com>
2025-06-20 16:03:31 +0545

    change

--- middlewares/authCheck.js
+++ middlewares/authCheck.js
@@ -1,15 +1,31 @@
 import jwt from "jsonwebtoken";
 
+// export const userCheck = (req, res, next) => {
+//   // const token = req.headers.authorization;
+//   const token = req.cookies?.jwt;
+//   const decode = jwt.decode(token, 'token');
+//   if (decode) {
+//     req.id = decode.id;
+//     req.isAdmin = decode.isAdmin;
+//     next();
+//   } else {
+//     return res.status(401).json({ message: 'unauthorized user' });
+//   }
+// }
+
 export const userCheck = (req, res, next) => {
-  // const token = req.headers.authorization;
   const token = req.cookies?.jwt;
-  const decode = jwt.decode(token, 'token');
-  if (decode) {
+  if (!token) {
+    return res.status(401).json({ message: 'No token provided' });
+  }
+
+  try {
+    const decode = jwt.verify(token, 'token');
     req.id = decode.id;
     req.isAdmin = decode.isAdmin;
     next();
-  } else {
-    return res.status(401).json({ message: 'unauthorized user' });
+  } catch (err) {
+    return res.status(401).json({ message: 'Invalid token' });
   }
 }
 

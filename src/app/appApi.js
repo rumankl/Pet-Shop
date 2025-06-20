b26f781a75fc55844099f4a78e6fae0f310e25f2
rumankl <rumankrishnalohala@gmail.com>
2025-06-20 14:22:53 +0545

    change

--- src/app/appApi.js
+++ src/app/appApi.js
@@ -2,30 +2,16 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { baseUrl } from "../data/apis";
 
 
-// export const appapi = createApi({
-//   reducerPath: "appapi",
-//   baseQuery: fetchBaseQuery({
-//     baseUrl: baseUrl,
-//     credentials: "include",
-//     headers: {
-//       "Content-Type": "application/json",
-//     }
-//   }),
-//   endpoints: (builder) => ({}),
-// })
-
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { baseUrl } from "../data/apis";
-
 export const appapi = createApi({
   reducerPath: "appapi",
   baseQuery: fetchBaseQuery({
     baseUrl: baseUrl,
     credentials: "include",
-    prepareHeaders: (headers) => {
-      headers.set("Content-Type", "application/json");
-      return headers;
-    },
+    headers: {
+      "Content-Type": "application/json",
+    }
   }),
   endpoints: (builder) => ({}),
-});
+})
+
+

9dc964e4909e40346a6a424f3b7367264552dbb7
rumankl <rumankrishnalohala@gmail.com>
2025-06-20 13:14:37 +0545

    change

--- src/UI/ShowDialog.jsx
+++ src/UI/ShowDialog.jsx
@@ -16,7 +16,7 @@ export function ShowDialog({ totalAmount, orderItems }) {
   const [addOrder, { isLoading }] = useAddOrderMutation();
   const [open, setOpen] = React.useState(false);
 
-  // const { user } = useSelector((state) => state.userSlice);
+  const { user } = useSelector((state) => state.userSlice);
 
   const handleOpen = () => setOpen(!open);
 
@@ -25,7 +25,7 @@ export function ShowDialog({ totalAmount, orderItems }) {
     try {
       await addOrder({
         body: { totalAmount, orderItems },
-        // token: user.token
+        token: user.token
         // token: user.token
       }).unwrap();
       toast.success('success');

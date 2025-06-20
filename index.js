f012267e668a543da3c1fb4628cc52e2bee2288c
rumankl <rumankrishnalohala@gmail.com>
2025-06-20 14:23:52 +0545

    change

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ const app = express();
 
 app.use(cors({
   // origin: ['https://pet-shop-navy.vercel.app', 'http://localhost:3000',],
-  origin: ['https://pet-shop-3bkk.vercel.app'],
+  origin: ['https://pet-shop-3bkk.vercel.app', 'http://localhost:3000'],
   // methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
 ////extra cors options if cors policy not working
 app.options('*', cors({
-  origin: ['https://pet-shop-3bkk.vercel.app'],
+  origin: ['https://pet-shop-3bkk.vercel.app', 'http://localhost:3000'],
   credentials: true
 }));
 

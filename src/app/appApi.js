7d7830934bd161a7278c03437f968f51279a8b4d
rumankl <rumankrishnalohala@gmail.com>
2025-06-20 15:04:59 +0545

    change

--- src/app/appApi.js
+++ src/app/appApi.js
@@ -8,6 +8,7 @@ export const appapi = createApi({
     baseUrl: baseUrl,
     credentials: "include",
     headers: {
+      Authorization: `Bearer ${token}`,
       "Content-Type": "application/json",
     }
   }),

09dfd17599468de26c4bad6aa7b64d0589b677f1
rumankl <rumankrishnalohala@gmail.com>
2025-06-20 16:30:52 +0545

    change

--- middlewares/authCheck.js
+++ middlewares/authCheck.js
@@ -2,6 +2,7 @@ import jwt from "jsonwebtoken";
 
 export const userCheck = (req, res, next) => {
   // const token = req.headers.authorization;
+  console.log('Received cookies:', req.cookies);
   const token = req.cookies?.jwt;
   const decode = jwt.decode(token, 'token');
   if (decode) {
